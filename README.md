f5c43f0bd01aa0a891d90ddbc64e2150876c53a8
Lukas Blakk <lukasblakk@gmail.com>
2015-02-01 23:46:53 +0100

    adjusted to be a relman bot v1.0

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-fennecbot
+relmanbot
 =========
 
-Friendly robot to help Firefox for Android developers in #mobile on irc.mozilla.org.
+Friendly robot (based on @mleibovic's fennecbot) to help release management in #relman on irc.mozilla.org.
 
 To install / run:
 
-* git clone git@github.com:leibovic/fennecbot.git
-* cd fennecbot
-* npm install
+* git clone git@github.com:lsblakk/relmanbot.git
+* cd relmanbot
+* npm install 
 * node server.js
 
 To be polite, please create your own private IRC channel for testing.
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var bz = require("bz"),
     irc = require("irc"),
     https = require("https"),
-    notes = require("./notes");
     config = require("./config");
 
 if (module.parent) {
@@ -16,67 +15,130 @@ var bot = new irc.Client(config.server, config.botName, {
 
 var bugzilla = bz.createClient();
 
-// Finds a bug that matches the search term, and says it to the person who asked about it.
-function findBug(from, to, search) {
-  bugzilla.searchBugs({ quicksearch: search }, function(error, bugs) {
+
+// Get a specific bug's summary & link
+function bugSummary(from, to, bugID) {
+  bugzilla.getBug(bugID, function(error, bug) {
+    if (!error) {
+      var summary = from + ": Bug " + bug.id + " - " + bug.summary + " http://bugzil.la/" + bug.id + " - " + "Assigned To: " + bug.assigned_to.real_name;
+      bot.say(to, summary);
+    }
+  });
+}
+
+// Get bug list from search criteria, count and URL
+function bugCountList(from, to, search) {
+  bugzilla.searchBugs(search, function(error, bugs) {
     if (error) {
       console.log(error);
       return;
     }
 
-    // Find a random bug from the array.
-    var index = Math.floor(Math.random() * bugs.length);
-    var bug = bugs[index];
-    console.log(bot.nick + " found bug " + bug.id);
+    bot.say(to, from + ": There are " + bugs.length + " public bugs for that request.");
+    var url = "https://bugzilla.mozilla.org/buglist.cgi?" + bugzilla.urlEncode(search);
+    bot.say(to,  "LINK: " + url);
 
-    var message = from + ": Try working on bug " + bug.id + " - " + bug.summary + " http://bugzil.la/" + bug.id;
-    bot.say(to, message);
+    return;
   });
 }
 
 bot.addListener("message", function(from, to, message) {
+  if (message.indexOf(bot.nick) === 0) {
+    if (message.split(/[, ]+/).length === 1){
+      bot.say(to, "Hi!  What do you need?");
+      return;
+    }
+  }
+
   if (message.indexOf(bot.nick) !== 0) {
     return;
   }
   
-  if (message.indexOf("tracking bug") > -1) {
-    findBug(from, to, "blocking-fennec:+ @nobody");
+  if (message.indexOf("bug") > -1) {
+    console.log("BugID: " + message.split(/[, ]+/).pop());
+    var bugID = message.split(/[, ]+/).pop();
+    bugSummary(from, to, bugID);
     return;
   }
 
-  if (message.indexOf("mentor bug") > -1) {
-    findBug(from, to, "prod:android sw:mentor @nobody");
+  if (message.indexOf("own") > -1) {
+    var user = "";
+    if (message.split(/[, ]+/).length > 2) {
+        var user = message.split(/[, ]+/).pop() + ": ";
+    }
+    bot.say(to, user + "Release owners listed at https://wiki.mozilla.org/Release_Management/Release_owners");
     return;
   }
 
-  if (message.indexOf("help") > -1) {
-    bot.say(to, from + ": Try looking at our Get Involved page https://wiki.mozilla.org/Mobile/Get_Involved");
+  // link to the public calendar of merge & release dates
+  if (message.indexOf("cal") > -1) {
+    bot.say(to, from + ": Release & Merge Day calendar http://bit.ly/1DlbjVc");
     return;
   }
 
-  if (message.indexOf("triage") > -1) {
-    bot.say(to, "https://wiki.mozilla.org/Mobile/Triage");
+  // link to the release manager calendar (more detailed)
+  if (message.indexOf("sched") > -1) {
+    bot.say(to, from + ": Release Schedule calendar http://bit.ly/1uQbPWI");
     return;
   }
 
+  // count & url for bugs tracking_$version: + 
+  // TODO: verification of version number (validation of digit)
+  if (message.indexOf("track") > -1) {
+    // TODO: esr needs a _ in front, eg: cf_status_firefox_esr31
+    var version = message.split(/[, ]+/).pop();
+    var tracking = "cf_tracking_firefox" + version
+    var status = "cf_status_firefox" + version
+    // For a specific version, in general
+    var searchParams = {
+      "field0-0-0" : status,
+      "value0-0-0" : "unaffected,fixed,verified,disabled,wontfix",
+      "type0-0-0" : "nowords"
+    };
+    searchParams[tracking] = "+";
+    bugCountList(from, to, searchParams)
+    return;
+  }
+
+  // count & url for bugs tracking_$version: ?
+  if (message.indexOf("nom") > -1) {
+    // TODO: esr needs a _ in front, eg: cf_status_firefox_esr31
+    var version = message.split(/[, ]+/).pop();
+    var tracking = "cf_tracking_firefox" + version
+    // For a specific version, in general
+    var searchParams = {};
+    searchParams[tracking] = "?";
+    bugCountList(from, to, searchParams)
+    return;
+  }
+
+  // tracking XX version, how many assigned to nobody
+  if (message.indexOf("tracking nobody") > -1) {
+    // TODO: esr needs a _ in front, eg: cf_status_firefox_esr31
+    var version = message.split(/[, ]+/).pop();
+    var tracking = "cf_tracking_firefox" + version
+    var status = "cf_status_firefox" + version
+
+    var searchParams = {
+      "field0-0-0" : status,
+      "value0-0-0" : "unaffected,fixed,verified,disabled,wontfix",
+      "type0-0-0" : "nowords",
+      "field1-0-0" : "assigned_to",
+      "value1-0-0" : "nobody",
+      "type1-0-0" : "substring"
+    }
+
+    searchParams[tracking] = "+";
+    bugCountList(from, to, searchParams);
+    return;
+  }
+
+  // latest release notes
   if (message.indexOf("notes") > -1) {
-    var recentNotes = notes.recent(from, to);
-    bot.say(to, recentNotes);
-    return;
-  }
+    var notes = "https://www.mozilla.org/firefox/notes\n";
+    notes += "https://www.mozilla.org/mobile/notes";
 
-  if (message.indexOf("build") > -1) {
-    bot.say(to, "https://wiki.mozilla.org/Mobile/Fennec/Android");
-    return;
-  }
-
-  if (message.indexOf("source") > -1) {
-    bot.say(to, "https://github.com/leibovic/fennecbot");
-    return;
-  }
-
-  if (message.indexOf("devices") > -1) {
-    bot.say(to, "https://wiki.mozilla.org/Mobile/Fennec/DeviceList");
+    bot.say(to,from + ": " + notes);
     return;
   }
 });
